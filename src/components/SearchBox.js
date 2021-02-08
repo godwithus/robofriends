1064c047e9ce713f6c1fcc73c1b0787c3e5d94f0
Emmanuel Adeyemo <emmanueladeyemo002@gmail.com>
2021-02-08 17:00:07 +0000

    Initial commit

---
+++ src/components/SearchBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const SearchBox = ({searchField, searchChange}) => {
+    return (
+        <div className='pa2'>
+            <input 
+                type='search' 
+                placeholder='Search Robot' 
+                className='pa2 ba b--green bg-lightest-blue'
+                onChange={searchChange}
+            />
+        </div>
+    );
+}
+
+export default SearchBox;
