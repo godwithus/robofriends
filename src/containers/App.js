2773c94738da9c099fd2e339c5b64e6c48b2501b
Emmanuel Adeyemo <emmanueladeyemo002@gmail.com>
2021-02-09 08:16:02 +0000

    Error Handling component added

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 class App extends Component {
   constructor(){
@@ -50,7 +51,9 @@ class App extends Component {
           <h1 className='h1'> RoboFriends </h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filteredRobots}/>
+            <ErrorBoundary>
+              <CardList robots={filteredRobots}/>
+            </ErrorBoundary>
           </Scroll>
         </div>
       );
